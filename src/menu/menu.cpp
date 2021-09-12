d69dbe3220d0a385a30c2d6da913867f0aa12d25
YaSanyaBeats <75445623+YaSanyaBeats@users.noreply.github.com>
2021-09-12 11:11:46 +0700

    events on menu buttons

--- src/menu/menu.cpp
+++ src/menu/menu.cpp
@@ -17,18 +17,54 @@ void initMenu(sf::RenderWindow& window)
     Text textButton;
     textButton.setFont(font);
     textButton.setCharacterSize(48);
-    textButton.setFillColor(sf::Color::White);
+    textButton.setFillColor(Color::White);
 
-    RectangleShape menuButton(Vector2f(500, 100));
-    menuButton.setFillColor(sf::Color(0, 0, 0));
-    menuButton.setOutlineColor(sf::Color(250, 150, 100));
-    menuButton.setOutlineThickness(5);
+    RectangleShape menuButton1(Vector2f(500, 100));
+    RectangleShape menuButton2(Vector2f(500, 100));
+
+    menuButton1.setFillColor(Color(0, 0, 0));
+    menuButton1.setOutlineColor(Color(250, 150, 100));
+    menuButton1.setOutlineThickness(5);
+    menuButton1.setPosition(Vector2f(710, 400));
+
+    menuButton2.setFillColor(Color(0, 0, 0));
+    menuButton2.setOutlineColor(Color(250, 150, 100));
+    menuButton2.setOutlineThickness(5);
+    menuButton2.setPosition(Vector2f(710, 550));
 
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
                 window.close();
+            }
+            FloatRect boundsButton1 = menuButton1.getGlobalBounds();
+            FloatRect boundsButton2 = menuButton2.getGlobalBounds();
+            if (event.type == sf::Event::MouseMoved) {
+                if (boundsButton1.contains(
+                            event.mouseMove.x, event.mouseMove.y)) {
+                    menuButton1.setFillColor(Color(250, 150, 100));
+                } else {
+                    menuButton1.setFillColor(Color(0, 0, 0));
+                }
+                if (boundsButton2.contains(
+                            event.mouseMove.x, event.mouseMove.y)) {
+                    menuButton2.setFillColor(Color(250, 150, 100));
+                } else {
+                    menuButton2.setFillColor(Color(0, 0, 0));
+                }
+            }
+            if (event.type == Event::MouseButtonPressed) {
+                if (event.mouseButton.button == Mouse::Left) {
+                    if (boundsButton1.contains(
+                                event.mouseButton.x, event.mouseButton.y)) {
+                        cout << "Start game";
+                    }
+                    if (boundsButton2.contains(
+                                event.mouseButton.x, event.mouseButton.y)) {
+                        window.close();
+                    }
+                }
             }
         }
 
@@ -38,11 +74,9 @@ void initMenu(sf::RenderWindow& window)
         window.draw(textButton);
 
         //отрисовываем кнопки
-        menuButton.setPosition(Vector2f(710, 400));
-        window.draw(menuButton);
 
-        menuButton.setPosition(Vector2f(710, 550));
-        window.draw(menuButton);
+        window.draw(menuButton1);
+        window.draw(menuButton2);
 
         //отрисовываем текст на кнопках
         textButton.setPosition(Vector2f(880, 420));
