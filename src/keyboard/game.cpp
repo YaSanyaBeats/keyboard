08057484502c85a144056ec2d70160d5609c327a
YaSanyaBeats <75445623+YaSanyaBeats@users.noreply.github.com>
2021-09-18 18:04:16 +0700

    lightning keyboard

--- src/keyboard/game.cpp
+++ src/keyboard/game.cpp
@@ -12,21 +12,25 @@ struct button {
     int number;
     Vector2f position;
     string folder;
+    string letter;
     bool active;
 };
 
 int getSymbolWidth(Text text)
 {
-    cout << text.findCharacterPos(1).x << endl;
     return text.findCharacterPos(2).x - text.findCharacterPos(1).x;
 }
 
 vector<button> getButtons()
 {
+    vector<string> letters
+            = {"q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d",
+               "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"};
     vector<button> buttons;
     for (int i = 1; i < 27; i++) {
         button currentButton;
         currentButton.number = i;
+        currentButton.letter = letters[i - 1];
         if (i <= 10) {
             currentButton.position = Vector2f(i * 70 + 600, 700);
         } else if (i <= 19) {
@@ -36,6 +40,7 @@ vector<button> getButtons()
         }
         string fold = "source/keys/" + to_string(i) + ".png";
         currentButton.folder = fold;
+        currentButton.active = false;
         buttons.push_back(currentButton);
     }
     return buttons;
@@ -52,8 +57,21 @@ void drawButtons(RenderWindow& window, vector<button> buttons)
         currentButton.setSize(Vector2f(60, 60));
         currentButton.setPosition(buttons[i].position);
         currentButton.setTexture(&texture);
+        if (buttons[i].active) {
+            currentButton.setFillColor(Color::Yellow);
+        }
         window.draw(currentButton);
     }
+}
+
+int getActiveButtonId(unsigned int unicode, vector<button> buttons)
+{
+    for (unsigned int i = 0; i < buttons.size(); i++) {
+        if (buttons[i].letter == static_cast<char>(unicode)) {
+            return i;
+        }
+    }
+    return -1;
 }
 
 void startGame(sf::RenderWindow& window)
@@ -81,6 +99,8 @@ void startGame(sf::RenderWindow& window)
     currentSymbol.setPosition(Vector2f(949, 300));
     currentSymbol.setFillColor(Color(250, 150, 100, 160));
 
+    int activeButtonId = 0;
+
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
@@ -89,6 +109,12 @@ void startGame(sf::RenderWindow& window)
             }
             if (event.type == sf::Event::TextEntered) {
                 if (event.text.unicode < 128) {
+                    buttons[activeButtonId].active = false;
+                    activeButtonId
+                            = getActiveButtonId(event.text.unicode, buttons);
+                    if (activeButtonId >= 0) {
+                        buttons[activeButtonId].active = true;
+                    }
                     if (static_cast<char>(event.text.unicode) == levelStr[0]) {
                         levelStr.erase(levelStr.begin());
                         currentSymbol.setSize(
